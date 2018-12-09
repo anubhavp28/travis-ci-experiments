84746d51b81e2159f1b11094281115920cd227ad
Anubhav Patel <anubhavp28@gmail.com>
2018-12-09 15:29:06 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify
+import requests as req
+
+app = Flask(__name__)
+
+@app.route('/series/current', methods=['GET'])
+def current_series():
+    try:
+        r = req.get('https://cricbuzz.com/api/match/current')
+        matches = r.json()
+    except ValueError:
+        return jsonify({
+            "error" : "Invalid json response received from cricbuzz servers. Cricbuzz might have changed their internal API."
+        })
+    except req.RequestException:
+        
+
+    uniques = set()
+    series = []
+    for match in matches:
+        if 'series' in match:
+            if match['series']['id'] not in uniques:
+                uniques.add(match['series']['id'])
+                series.append(match['series'])
+
+    return jsonify(series)
+
+@app.route('/match/current', methods=['GET'])
+def current_match():
